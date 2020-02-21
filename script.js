7aa376907d6e1e50db251bec478ee39f925c988f
ch4n3-yoon <ch4n3.yoon@gmail.com>
2020-02-22 01:56:01 +0900

    Second commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 
-chrome.tabs.executeScript({
-	file: 'exploit.js'
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript({
+		file: "exploit.js",
+		allFrames: false
+	});
 });
 
+// location.href = "javascript:document.getElementById('Main').contentDocument.getElementById('coreMain').contentWindow.msgAlert('hihi')";
