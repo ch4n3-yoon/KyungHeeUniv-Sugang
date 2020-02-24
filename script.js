b90297aeacfe6aec66f2100dc4812345e9503544
ch4n3-yoon <ch4n3.yoon@gmail.com>
2020-02-24 12:39:28 +0900

    sugang.khu.ac.kr에서만 동작하도록 설정 & 페이지 로드되면 바로 무효화하도록 설정

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript({
-		file: "exploit.js",
-		allFrames: false
+if (document.domain == "sugang.khu.ac.kr") {
+	chrome.tabs.onUpdated.addListener(function(tab) {
+		chrome.tabs.executeScript({
+			file: "exploit.js",
+			allFrames: false
+		});
 	});
-});
-
-// location.href = "javascript:document.getElementById('Main').contentDocument.getElementById('coreMain').contentWindow.msgAlert('hihi')";
+}
